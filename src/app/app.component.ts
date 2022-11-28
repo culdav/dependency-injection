6f3ef2137e14cf7a6ddacddbe0906cf546f57e62
culdav <culdavlad93@gmail.com>
2022-11-28 16:35:40 +0200

    Added factory example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppConfig, APP_CONFIG } from './config';
 import { ExperimentalLoggerService } from './experimental-logger.service';
 import { Component, OnInit } from '@angular/core';
 import { LoggerService } from './logger.service';
@@ -7,7 +8,17 @@ import { LegacyLogger } from 'src/app/logger.legacy';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [{ provide: LoggerService, useValue: LegacyLogger }],
+  providers: [
+    {
+      provide: LoggerService,
+      useFactory: (appConfig: AppConfig) => {
+        return appConfig.experimentalEnabled
+          ? new ExperimentalLoggerService()
+          : new LoggerService();
+      },
+      deps: [APP_CONFIG],
+    },
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'dependency-injection';
